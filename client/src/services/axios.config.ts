3d0a95faf66a8617867ebd6f3266c04fc993532e
Sean Caleb Ubias <snclbmnzubs@gmail.com>
2022-10-02 20:51:53 +0800

    configure axios defaults

--- client/src/services/axios.config.ts
+++ client/src/services/axios.config.ts
@@ -1,13 +1,34 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-let url;
+type Token = string | null;
+interface ServiceRequestConfigs {
+    token?: Token;
+    options: AxiosRequestConfig;
+}
 
-if (import.meta.env.PROD) url = import.meta.env.VITE_PROD_URL;
-else url = import.meta.env.VITE_DEV_URL;
+const baseURL = import.meta.env.PROD
+    ? import.meta.env.VITE_PROD_URL
+    : import.meta.env.VITE_DEV_URL;
 
-export default axios.create({
-    baseURL: `${url}/api`,
-    headers: {
-        'Content-type': 'application/json',
-    },
+const client = axios.create({
+    baseURL,
 });
+
+export default async function <T>({
+    token,
+    options,
+}: ServiceRequestConfigs): Promise<T> {
+    if (token) {
+        client.defaults.headers.common.Authorization = `Bearer ${token}`;
+    }
+
+    const onSuccess = (response: AxiosResponse<T>) => response.data;
+    const onError = (err: Error) => err;
+
+    try {
+        return onSuccess(await client(options));
+    } catch (error) {
+        const err = error as Error;
+        throw onError(err);
+    }
+}
